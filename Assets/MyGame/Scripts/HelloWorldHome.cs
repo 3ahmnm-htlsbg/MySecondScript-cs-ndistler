bedc54a2de43bb684f5911993bf775e81e3171dd
niklasdistler <niklasdistler@Niklass-MacBook-Pro.local>
2019-11-02 12:55:57 +0100

    init Project

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -7,12 +7,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      Debug.Log("Hello World at home")  
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+           
