339ddaa66b81afa5ccd58c1f327b6a2119e3afe1
niklasdistler <niklasdistler@Niklass-MacBook-Pro.local>
2019-11-02 13:08:48 +0100

    init Project

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HelloWorldHome : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-      Debug.Log("Hello World at home")  
-    }
-           
+        Debug.Log("Hello World at home");
+        }
+}          
